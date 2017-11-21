20037d390dba94d04acf79a62e49d934591806bb
Diego Pineda <dpineda@chewy.com>
2017-11-20 21:52:54 -0500

    solved n23

--- src/ii_collections/n23CompoundTasks.kt
+++ src/ii_collections/n23CompoundTasks.kt
@@ -1,18 +1,20 @@
 package ii_collections
 
-fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
-    // Return the set of customers who ordered the specified product
-    todoCollectionTask()
-}
+fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> =
+        this.customers.filter { it.orderedProducts.contains(product) }.toSet()
 
-fun Customer.getMostExpensiveDeliveredProduct(): Product? {
-    // Return the most expensive product among all delivered products
+
+fun Customer.getMostExpensiveDeliveredProduct(): Product? =
+        // Return the most expensive product among all delivered products
     // (use the Order.isDelivered flag)
-    todoCollectionTask()
-}
+    this.orders.filter { it.isDelivered }
+            .flatMap { it.products }
+            .sortedByDescending { it.price }
+            .get(0)
 
-fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
+fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int =
     // Return the number of times the given product was ordered.
     // Note: a customer may order the same product for several times.
-    todoCollectionTask()
-}
+    this.customers.flatMap { it.orders.flatMap { it.products } }
+            .filter { it.equals(product) }
+            .count()
